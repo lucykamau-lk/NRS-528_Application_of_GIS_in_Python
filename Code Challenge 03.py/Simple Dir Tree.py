fff191840e371231c6c4c52d87332692df93ce2e
Lucy Kamau <157840001+lucykamau-lk@users.noreply.github.com>
2024-02-10 01:47:01 -0500

    Update Simple Dir Tree.py
    Add New File

--- Code Challenge 03.py/Simple Dir Tree.py
+++ Code Challenge 03.py/Simple Dir Tree.py
@@ -1,1 +1,32 @@
+import os
+import shutil
 
+# Define the directory structure
+directories = [
+    "draft_code/pending",
+    "draft_code/complete",
+    "includes",
+    "layouts/default",
+    "layouts/post/posted",
+    "site"
+]
+
+# Create directories
+for directory in directories:
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno == os.errno.EEXIST:  # Directory already exists
+            pass
+        else:
+            raise
+
+print("Directory tree created successfully.")
+
+# Delete directories
+for directory in directories:
+    try:
+        shutil.rmtree(directory)
+        print("Directory '{directory}' deleted successfully.")
+    except OSError as e:
+        print("Error: {e}")
