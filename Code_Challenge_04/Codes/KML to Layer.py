486fde0162065716206ce5168efbbf95a32a0da2
Lucy Kamau <157840001+lucykamau-lk@users.noreply.github.com>
2024-03-09 08:00:40 -0500

    Update KML to Layer.py

--- Code_Challenge_04/Codes/KML to Layer.py
+++ Code_Challenge_04/Codes/KML to Layer.py
@@ -2,22 +2,11 @@ import arcpy
 import os
 
 def convert_data(kml_file, output_folder):
-    """
-    Converts a KML file to a layer file in ArcGIS.
-
-    Args:
-    kml_file (str): Path to the input KML file.
-    output_layer (str): Path to save the output layer file.
-    """
-    # Set up the workspace environment
-    arcpy.env.workspace = output_folder
-
-    # Output layer file name
-    output_layer = os.path.join(output_folder, "output_layer.lyr")
-
+    pass
     try:
-        # Perform data conversion
-        arcpy.KMLToLayer_conversion(kml_file,output_folder)
+        arcpy.env.workspace = output_folder
+        os.path.join(output_folder, "output_layer.lyr")
+        arcpy.KMLToLayer_conversion(kml_file, output_folder)
 
         print("Conversion completed successfully.")
     except arcpy.ExecuteError:
@@ -30,15 +19,10 @@ def convert_data(kml_file, output_folder):
 
 if __name__ == "__main__":
     # Example input KML file path - replace with your actual file path
-    kml_file_path = r"C:\GitHub\NRS_528\Code Challenge 04\Watershed.kml"
+    kml_file_path = r"C:\GitHub\NRS_528\Trial_04\Watershed.kml"
 
     # Output layer file path - replace with your desired output layer path
-    output_folder_path = r"C:\GitHub\NRS_528\Code Challenge 04\Results"
-
-
-    # Create output folder if it doesn't exist
-    if not os.path.exists(output_folder_path):
-        os.makedirs(output_folder_path)
+    output_folder_path = r"C:\GitHub\NRS_528\Trial_04\KML to Layer"
 
     # Call the conversion function
     convert_data(kml_file_path, output_folder_path)
